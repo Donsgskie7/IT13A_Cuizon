026602bff5dfc382934dcf7132fc8364051eea4c
JOHNANDERSON7 <johnanderson312006@gmail.com>
2025-03-11 20:41:34 +0800

    Update Cuizon_Lab2_Act_Palindrome.java

--- IT13A_CUIZON_Activity/Cuizon_Lab2_Act_Palindrome.java
+++ IT13A_CUIZON_Activity/Cuizon_Lab2_Act_Palindrome.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Midterms;
     import java.util.Scanner;
-/**
- *
- * @author sagui
- */
+
 public class Lab2_Act_Palindrome {
     public static void main (String [] args){
     
